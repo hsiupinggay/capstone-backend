db7c77825fab4847db406617cf6958fdce9e854c
shannonssd <shannonssd@gmail.com>
2022-03-06 23:11:18 +0800

    Created public and uploads folder, added multer to routers

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -7,10 +7,13 @@
  * ========================================================
  * ========================================================
  */
-
 const express = require('express');
+const multer = require('multer');
 
+// Set name of photo upload directory
+const multerUpload = multer({ dest: './public/uploads' });
 const router = express.Router();
+
 /*
  * ========================================================
  * ========================================================
