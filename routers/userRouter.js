f920da6998e05ba67a760d23f21d194f0650ea7b
Hsiu Ping Gay <hsiups@192.168.1.5>
2022-03-10 20:46:43 +0700

    deconflict

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -29,11 +29,8 @@ module.exports = function userRouter(controller) {
   router.post('/login', controller.login.bind(controller));
   router.post('/signup', controller.signup.bind(controller));
   router.put('/profile', authToken(), controller.editProfile.bind(controller));
-<<<<<<< HEAD
   router.get('/authenticate', controller.authenticate.bind(controller));
-=======
   router.get('/all-family', controller.findFamily.bind(controller));
 
->>>>>>> 5a76a0e2d99e9bc742ec537a50f5827b381ad3cc
   return router;
 };
