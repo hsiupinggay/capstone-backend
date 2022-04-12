0dfa3caa24db605b53f7d0bb787b17cdbfbfa0d9
Hsiu Ping Gay <hsiups@Hsius-MacBook-Pro.local>
2022-04-12 16:06:32 +0800

    error handle message controller

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -29,39 +29,46 @@ class MessageController extends BaseController {
   }
 
   async getMessage(req, res) {
-    console.log('req.body', req.body);
-    const chatId = req.body.message.chat.id;
-    if (req.body.message.text === '/patient') {
-      await axios.post(`${TELEGRAM_API}/sendMessage`, {
-        chat_id: chatId,
-        text: 'Enter the patient id string',
-        reply_markup: JSON.stringify({ force_reply: true }),
-      });
+    if (!req.body.message) {
+      this.errorHandler(res, 400, { error: 'oops, something went wrong' });
     }
 
-    if (req.body.message.reply_to_message) {
-      if (req.body.message.reply_to_message.text === 'Enter the patient id string') {
-        const { text } = req.body.message;
-        const { id, username } = req.body.message.chat;
-        const patientId = text.toString();
-        const telegramUserId = id.toString();
-        const userName = username.toString();
-        // find patient in db to add telegram user to patient
-        const patient = await this.model.findOne({ _id: patientId });
-
-        // get patient name for customised message to confirm subscription
-        const patientName = patient.identity.name.first;
-
-        patient.medEmailList.push({ email: telegramUserId, name: userName });
-        await patient.save();
-
+    try {
+      const chatId = req.body.message.chat.id;
+      if (req.body.message.text === '/patient') {
         await axios.post(`${TELEGRAM_API}/sendMessage`, {
-          chat_id: id,
-          text: `You are subscribed to ${patientName}'s medication refill reminders`,
+          chat_id: chatId,
+          text: 'Enter the patient id string',
+          reply_markup: JSON.stringify({ force_reply: true }),
         });
       }
+
+      if (req.body.message.reply_to_message) {
+        if (req.body.message.reply_to_message.text === 'Enter the patient id string') {
+          const { text } = req.body.message;
+          const { id, username } = req.body.message.chat;
+          const patientId = text.toString();
+          const telegramUserId = id.toString();
+          const userName = username.toString();
+          // find patient in db to add telegram user to patient
+          const patient = await this.model.findOne({ _id: patientId });
+
+          // get patient name for customised message to confirm subscription
+          const patientName = patient.identity.name.first;
+
+          patient.medEmailList.push({ email: telegramUserId, name: userName });
+          await patient.save();
+
+          await axios.post(`${TELEGRAM_API}/sendMessage`, {
+            chat_id: id,
+            text: `You are subscribed to ${patientName}'s medication refill reminders`,
+          });
+        }
+      }
+    } catch (err) {
+      return this.errorHandler(res, 400, { err });
     }
-    this.successHandler(res, 200, { telebotRunning: true });
+    return this.successHandler(res, 200, { telebotRunning: true });
   }
 }
 
